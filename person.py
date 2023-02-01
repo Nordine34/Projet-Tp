a3d510cbbf1bfd2c104b2be101f75c90b8eb1c5f
Nour-Eddine ZAIDI <106592846+Nordine34@users.noreply.github.com>
2023-02-01 20:56:50 +0100

    Create person.py

---
+++ person.py
@@ -0,0 +1,11 @@
+class Personne:
+    id:int
+    firstName:str
+    lastName:str
+    solde:float
+
+    def __init__(self, id, firstName, lastName, solde):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.solde = solde
